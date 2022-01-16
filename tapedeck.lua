be8e7322f324dc5f0b50e7400d2aad5753624597
Zack Scholl <zack.scholl@gmail.com>
2022-01-16 09:27:25 -0800

    add pre

--- tapedeck.lua
+++ tapedeck.lua
@@ -1,5 +1,15 @@
--- tape effects
-
+-- tapedeck v0.1.0
+-- tape emulator fx
+--
+-- llllllll.co/t/tapedeck
+--
+--
+--
+--    ▼ instructions below ▼
+--
+-- K2/K3 toggles stages 
+-- E1/E2/E3 changes parameters
+--
 
 engine.name="Tapedeck"
 
@@ -85,7 +95,7 @@ function init()
 
   params:bang()
 
-  msg("MIX"..math.random(1,12),30)
+  msg("MIX "..ToRomanNumerals(math.random(1,12)),30)
 
   clock.run(function()
       while true do 
@@ -95,6 +105,46 @@ function init()
   end)
 end
 
+
+function ToRomanNumerals(s)
+  local map = { 
+    I = 1,
+    V = 5,
+    X = 10,
+    L = 50,
+    C = 100, 
+    D = 500, 
+    M = 1000,
+  }
+  local numbers = { 1, 5, 10, 50, 100, 500, 1000 }
+  local chars = { "I", "V", "X", "L", "C", "D", "M" }
+
+  
+  --s = tostring(s)
+  s = tonumber(s)
+  if not s or s ~= s then error"Unable to convert to number" end
+  if s == math.huge then error"Unable to convert infinity" end
+  s = math.floor(s)
+  if s <= 0 then return s end
+local ret = ""
+      for i = #numbers, 1, -1 do
+      local num = numbers[i]
+      while s - num >= 0 and s > 0 do
+          ret = ret .. chars[i]
+          s = s - num
+      end
+      --for j = i - 1, 1, -1 do
+      for j = 1, i - 1 do
+          local n2 = numbers[j]
+          if s - (num - n2) >= 0 and s < num and s > 0 and num - n2 ~= n2 then
+              ret = ret .. chars[j] .. chars[i]
+              s = s - (num - n2)
+              break
+          end
+      end
+  end
+  return ret
+end
 
 
 
